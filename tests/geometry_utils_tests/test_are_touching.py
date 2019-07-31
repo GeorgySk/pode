6ab0ac4f4a74a2cd3c9425595e90b2174e9978dc
LostFan123 <skorobogatov@phystech.edu>
2019-07-31 15:24:45 +0200

    Do not trim wkt output

--- tests/geometry_utils_tests/test_are_touching.py
+++ tests/geometry_utils_tests/test_are_touching.py
@@ -2,6 +2,7 @@ from typing import Tuple
 
 from hypothesis import (given,
                         note)
+from shapely import wkt
 from shapely.geometry import LineString
 
 from pode.geometry_utils import are_touching
@@ -13,27 +14,27 @@ from tests.strategies import (disjoint_lines,
 
 @given(lines_one_in_another)
 def test_one_in_another(lines: Tuple[LineString, LineString]) -> None:
-    note(f'Line 1: {lines[0]}\n'
-         f'Line 2: {lines[1]}')
+    note(f'Line 1: {wkt.dumps(lines[0], trim=False)}\n'
+         f'Line 2: {wkt.dumps(lines[1], trim=False)}')
     assert are_touching(*lines)
 
 
 @given(overlapping_lines)
 def test_overlapping(lines: Tuple[LineString, LineString]) -> None:
-    note(f'Line 1: {lines[0]}\n'
-         f'Line 2: {lines[1]}')
+    note(f'Line 1: {wkt.dumps(lines[0], trim=False)}\n'
+         f'Line 2: {wkt.dumps(lines[1], trim=False)}')
     assert are_touching(*lines)
 
 
 @given(touching_lines)
 def test_touching(lines: Tuple[LineString, LineString]) -> None:
-    note(f'Line 1: {lines[0]}\n'
-         f'Line 2: {lines[1]}')
+    note(f'Line 1: {wkt.dumps(lines[0], trim=False)}\n'
+         f'Line 2: {wkt.dumps(lines[1], trim=False)}')
     assert not are_touching(*lines)
 
 
 @given(disjoint_lines)
 def test_disjoint(lines: Tuple[LineString, LineString]) -> None:
-    note(f'Line 1: {lines[0]}\n'
-         f'Line 2: {lines[1]}')
+    note(f'Line 1: {wkt.dumps(lines[0], trim=False)}\n'
+         f'Line 2: {wkt.dumps(lines[1], trim=False)}')
     assert not are_touching(*lines)
