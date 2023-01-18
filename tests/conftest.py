3d79c8ede2f290245b39d30c98a00ece2af278b3
GeorgySk <skorobogatov@phystech.edu>
2023-01-18 22:46:01 +0100

    update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import pytest
 from hypothesis import (HealthCheck,
                         settings)
 
@@ -5,3 +6,10 @@ settings.register_profile('default',
                           deadline=None,
                           suppress_health_check=[HealthCheck.filter_too_much,
                                                  HealthCheck.too_slow])
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_sessionfinish(session: pytest.Session,
+                         exitstatus: pytest.ExitCode) -> None:
+    if exitstatus == pytest.ExitCode.NO_TESTS_COLLECTED:
+        session.exitstatus = pytest.ExitCode.OK
